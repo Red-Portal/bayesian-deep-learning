c2507f72e3d73355675bf948a4b623ed59de3ead
Ray Kim <msca8h@naver.com>
2020-02-10 14:51:21 +0900

    initial commit

---
+++ mnist.jl
@@ -0,0 +1,175 @@
+# Classifies MNIST digits with a convolutional network.
+# Writes out saved model to the file "mnist_conv.bson".
+# Demonstrates basic model construction, training, saving,
+# conditional early-exit, and learning rate scheduling.
+#
+# This model, while simple, should hit around 99% test
+# accuracy after training for approximately 20 epochs.
+
+using Flux, Flux.Data.MNIST, Statistics
+using Flux: onehotbatch, onecold, crossentropy, throttle
+using Metalhead: trainimgs, CIFAR10
+using Images: channelview
+using Base.Iterators: repeated, partition
+using Printf, BSON
+
+# Bundle images together with labels and group into minibatchess
+function make_minibatch(X, Y, idxs)
+    X_batch = Array{Float32}(undef, size(X[1])..., 1, length(idxs))
+    for i in 1:length(idxs)
+        X_batch[:, :, :, i] = Float32.(X[idxs[i]])
+    end
+    Y_batch = onehotbatch(Y[idxs], 0:9)
+    return (X_batch, Y_batch)
+end
+
+batch_size = 128
+
+# Load labels and images from Flux.Data.MNIST
+@info("Loading data set")
+train        = trainimgs(CIFAR10)
+mb_idxs      = partition(1:length(train_imgs), batch_size)
+train_imgs   = cat([getarray(train[i].img) for i in 1:50000]..., dims=4)
+train_labels = onehotbatch([train[i].ground_truth.class for i in 1:50000],1:10)
+train_set    = [make_minibatch(train_imgs, train_labels, i) for i in mb_idxs]
+
+# Prepare test set as one giant minibatch:
+val        = valimgs(CIFAR10)
+val_imgs   = cat([getarray(val[i].img) for i in 1:10000]..., dims=4)
+val_labels = onehotbatch([val[i].ground_truth.class for i in 1:10000], 1:10) |> gpu
+val_set    = make_minibatch(val_imgs, val_labels, 1:length(val_imgs))
+
+# Define our model.  We will use a simple convolutional architecture with
+# three iterations of Conv -> ReLU -> MaxPool, followed by a final Dense
+# layer that feeds into a softmax probability output.
+@info("Constructing model...")
+
+model = Chain(
+  Conv((3, 3), 3 => 64, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(64),
+  Conv((3, 3), 64 => 64, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(64),
+  MaxPool((2,2)),
+  Conv((3, 3), 64 => 128, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(128),
+  Conv((3, 3), 128 => 128, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(128),
+  MaxPool((2,2)),
+  Conv((3, 3), 128 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  MaxPool((2,2)),
+  Conv((3, 3), 256 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  MaxPool((2,2)),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  MaxPool((2,2)),
+  x -> reshape(x, :, size(x, 4)),
+  Dense(512, 1024, relu),
+  #Dropout(0.5),
+  Dense(4096, 124, relu),
+  #Dropout(0.5),
+  Dense(1024, 10),
+  softmax)
+
+# Load model and datasets onto GPU, if enabled
+train_set = gpu.(train_set)
+val_set   = gpu.(val_set)
+model     = gpu(model)
+
+# Make sure our model is nicely precompiled before starting our training loop
+model(train_set[1][1])
+
+# `loss()` calculates the crossentropy loss between our prediction `y_hat`
+# (calculated from `model(x)`) and the ground truth `y`.  We augment the data
+# a bit, adding gaussian random noise to our image to make it more robust.
+function loss(x, y)
+    # We augment `x` a little bit here, adding in random noise
+    x_aug = x .+ 0.1f0*gpu(randn(eltype(x), size(x)))
+
+    y_hat = model(x_aug)
+    return crossentropy(y_hat, y)
+end
+accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
+
+function Flux.train!(loss, ps, data, opt; cb = () -> ())
+  ps = Params(ps)
+  cb = runall(cb)
+  Threads.@threads for d in data
+    try
+      gs = gradient(ps) do
+        loss(d...)
+      end
+      update!(opt, ps, gs)
+      cb()
+    catch ex
+      if ex isa StopException
+        break
+      else
+        rethrow(ex)
+      end
+    end
+  end
+end
+
+
+# Train our model with the given training set using the ADAM optimizer and
+# printing out performance against the val set as we go.
+opt = ADAM(0.001)
+
+@info("Beginning training loop...")
+best_acc = 0.0
+last_improvement = 0
+for epoch_idx in 1:100
+    global best_acc, last_improvement
+    # Train for a single epoch
+
+    
+    x .-= apply!(opt, x, x̄)
+
+    Flux.train!(loss, params(model), train_set, opt)
+
+    # Calculate accuracy:
+    acc = accuracy(val_set...)
+    @info(@sprintf("[%d]: Val accuracy: %.4f", epoch_idx, acc))
+
+    # If our accuracy is good enough, quit out.
+    if acc >= 0.999
+        @info(" -> Early-exiting: We reached our target accuracy of 99.9%")
+        break
+    end
+
+    # If this is the best accuracy we've seen so far, save the model out
+    if acc >= best_acc
+        @info(" -> New best accuracy! Saving model out to mnist_conv.bson")
+        BSON.@save joinpath(dirname(@__FILE__), "mnist_conv.bson") model epoch_idx acc
+        best_acc = acc
+        last_improvement = epoch_idx
+    end
+
+    # If we haven't seen improvement in 5 epochs, drop our learning rate:
+    if epoch_idx - last_improvement >= 5 && opt.eta > 1e-6
+        opt.eta /= 10.0
+        @warn(" -> Haven't improved in a while, dropping learning rate to $(opt.eta)!")
+
+        # After dropping learning rate, give it a few epochs to improve
+        last_improvement = epoch_idx
+    end
+
+    if epoch_idx - last_improvement >= 10
+        @warn(" -> We're calling this converged.")
+        break
+    end
+end
