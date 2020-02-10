930c41098824f9755ec5e906ff4a6dc3ac0557cd
Ray Kim <msca8h@naver.com>
2020-02-10 20:27:58 +0900

    what?

--- mnist.jl
+++ mnist.jl
@@ -123,7 +123,7 @@ function Flux.train!(loss, ps, data, opt; cb = () -> ())
             end
         end
         for θ in ps
-            update!(opt, θ,  mean([batch[θ] for batch in batches]))
+            Flux.update!(opt, θ,  mean([batch[θ] for batch in batches]))
         end
     catch ex
       if ex isa StopException
